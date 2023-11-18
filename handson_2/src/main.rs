98760e20d25c6e80427ab0124dc9639c69f86517
MassimilianoBaglioni <3d.massimilianobaglioni@gmail.com>
2023-11-18 19:31:22 +0100

    created function to read from the test files

--- handson_2/src/main.rs
+++ handson_2/src/main.rs
@@ -6,17 +6,46 @@ struct SegmentTree {
 
 impl SegmentTree {
     fn new(size: u32) -> Self {
-        Self {
-            size,
-            tree: vec![0; 2 * size as usize],
-        }
+        let tree = vec![0; 2 * size as usize];
+
+        SegmentTree{size, tree}
+    }
+
+    fn init_tree(){
+
     }
 
     fn print_tree(&self) {
         println!("{:?}", self.tree);
     }
 }
+
+#[cfg(test)]
+mod tests {
+}
+
+fn run_tests(){
+    use std::fs;
+    use std::path::Path;
+    use std::path::PathBuf;
+
+    let filename = "input0.txt";
+    let directory_path = "src/Testset_handson2_2324_p1/";
+    let full_path = PathBuf::from(directory_path).join(Path::new(filename));
+
+    let contents = fs::read_to_string(full_path).expect("Failed to open the test file.");
+
+    let lines: Vec<&str> = contents.lines().collect();
+
+    for line in lines{
+        let values: Vec<i32> = line.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
+        
+        println!("{:?}", values);
+        }
+    }
+
 fn main() {
     let tree = SegmentTree::new(5);
     tree.print_tree();
+    run_tests()
 }
---
+++ handson_2/target/debug/deps/handson_2-9c00763bc38c50af.d
@@ -0,0 +1,5 @@
+/home/massimiliano/Documents/Coding/handsons/cpc-handsons/handson_2/target/debug/deps/handson_2-9c00763bc38c50af: src/main.rs
+
+/home/massimiliano/Documents/Coding/handsons/cpc-handsons/handson_2/target/debug/deps/handson_2-9c00763bc38c50af.d: src/main.rs
+
+src/main.rs:
