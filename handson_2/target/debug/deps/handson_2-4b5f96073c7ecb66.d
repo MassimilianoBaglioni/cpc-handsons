8340554a9db89ef1fa145ea4e3d19a0f6d2049a2
MassimilianoBaglioni <3d.massimilianobaglioni@gmail.com>
2023-11-18 18:52:07 +0100

    Forder structure fixed

---
+++ handson_2/target/debug/deps/handson_2-4b5f96073c7ecb66.d
@@ -0,0 +1,5 @@
+/home/massimiliano/Documents/Coding/handsons/cpc-handsons/handson_2/target/debug/deps/handson_2-4b5f96073c7ecb66: src/main.rs
+
+/home/massimiliano/Documents/Coding/handsons/cpc-handsons/handson_2/target/debug/deps/handson_2-4b5f96073c7ecb66.d: src/main.rs
+
+src/main.rs:
---
+++ handson_2/target/debug/handson_2.d
@@ -0,0 +1,1 @@
+/home/massimiliano/Documents/Coding/handsons/cpc-handsons/handson_2/target/debug/handson_2: /home/massimiliano/Documents/Coding/handsons/cpc-handsons/handson_2/src/main.rs
