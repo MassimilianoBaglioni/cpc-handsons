c80fcb3cf1384f196fcde789fd014c120e322b7e
MassimilianoBaglioni <3d.massimilianobaglioni@gmail.com>
2023-11-20 01:47:03 +0100

    stuck on max function big time

--- handson_2/src/main.rs
+++ handson_2/src/main.rs
@@ -1,30 +1,55 @@
 #[derive(Debug)]
 struct SegmentTree {
-    size: u32,
-    tree: Vec<u32>,
+    size: usize,
+    tree: Vec<i32>,
 }
 
 impl SegmentTree {
-    fn new(size: u32) -> Self {
-        let tree = vec![0; 2 * size as usize];
+    fn new(array: &[i32]) -> Self {
+        let mut tree = vec![0; 2 * array.len()];
+        tree[array.len()..].copy_from_slice(array);
 
-        SegmentTree { size, tree }
+        for i in (1..array.len()).rev() {
+            tree[i] = tree[i * 2].max(tree[i * 2 + 1]);
+        }
+
+        SegmentTree {
+            size: array.len(),
+            tree,
+        }
     }
-
-    fn init_tree() {}
 
     fn print_tree(&self) {
         println!("{:?}", self.tree);
     }
 
-    fn update(&self, i: i32, j: i32, t: i32) -> Vec<u32> {
+    fn update(&self, i: usize, j: usize, t: usize) -> Vec<u32> {
         println!("called update on {},{},{}", i, j, t);
         vec![0; 2]
     }
 
-    fn max(&self, i: i32, j: i32) -> i32 {
-        println!("called max on {},{},", i, j);
-        32
+    //TODO j is exclusive.
+    fn max(&self, mut i: usize, mut j: usize) -> i32 {
+        i += self.size;
+        j += self.size;
+
+        let mut max_val = std::i32::MIN;
+
+        while i < j {
+            if (i & 1) == 1 {
+                //i is odd
+                max_val = max_val.max(self.tree[i]);
+                i += 1;
+            }
+            if (j & 1) == 1 {
+                j -= 1;
+                max_val = max_val.max(self.tree[j]);
+            }
+            i /= 2;
+            j /= 2;
+        }
+        //println!("called max on {},{},", i, j);
+        max_val
     }
 }
 
@@ -37,7 +62,7 @@ fn run_tests() {
     use std::path::PathBuf;
 
     let directory_path = "src/Testset_handson2_2324_p1/";
-    let tree = SegmentTree::new(5);
+    let tree = SegmentTree::new(&[1, 2, 3, 4]);
 
     //TODO Hardcoded number of txt should refactor and count the number of txt files in the folder
     for i in 0..=10 {
@@ -77,9 +102,9 @@ fn run_tests() {
 
         for line in all_input_values.iter().skip(2) {
             if line[0] == 0 {
-                tree.update(line[1], line[2], line[3]);
+                tree.update(line[1] as usize, line[2] as usize, line[3] as usize);
             } else if line[0] == 1 {
-                tree.max(line[1], line[2]);
+                tree.max(line[1] as usize, line[2] as usize);
                 println!("assert {}", output_array[output_index]);
                 output_index += 1;
             }
@@ -88,7 +113,9 @@ fn run_tests() {
 }
 
 fn main() {
-    let tree = SegmentTree::new(5);
-    tree.print_tree();
-    run_tests()
+    let tree = SegmentTree::new(&[2, 1, 4, 3, 2]);
+
+    println!("{} max", tree.max(2, 3));
+    println!("{} max", tree.max(1, 2));
+    //run_tests();
 }
